f74b239b10f69c0e1bdee663f86c2526c463aa9e
Andrew Waugh <andrew.waugh@prov.vic.gov.au>
2023-11-03 12:44:22 +1100

    Added VEOFailure.java as part of the restructure of reporting in the V3Analysis

--- src/VERSCommon/VEOError.java
+++ src/VERSCommon/VEOError.java
@@ -11,6 +11,7 @@ package VERSCommon;
  * This VEO should be aborted and cleaned up, but further VEOs may be attempted.
  */
 public class VEOError extends Exception {
+
     String errorId; // unique identifier for type of error
     int errno;  // error number for automated processing
 
@@ -43,21 +44,36 @@ public class VEOError extends Exception {
      */
     
     /**
+     * Construct a new VEOError using a VEOFailure
+     *
+     * @param vf the VEOFailure
+     */
+    public VEOError(VEOFailure vf) {
+        super(vf.getMessage());
+        errorId = vf.getFailureId(); // has to be done twice because you need to call super() first
+        this.errno = 0;
+    }
+
+    /**
      * Construct a new VEOError from a class constructor giving error number
-      
-     * @param classname name of class and method generating the error (must not be null)
+     *
+     * @param classname name of class and method generating the error (must not
+     * be null)
      * @param errno unique error number (must be > 0)
+     * @param s the error message
      */
     public VEOError(String classname, int errno, String s) {
         super(errMesg(constErrorId(classname, null, errno), s, null));
         errorId = constErrorId(classname, null, errno); // has to be done twice because you need to call super() first
         this.errno = errno;
     }
-    
+
     /**
-     * Construct a new VEOError from a class constructor giving error number and an exception
-     * 
-     * @param classname name of class and method generating the error (must not be null)
+     * Construct a new VEOError from a class constructor giving error number and
+     * an exception
+     *
+     * @param classname name of class and method generating the error (must not
+     * be null)
      * @param errno unique error number (must be > 0)
      * @param s error message
      * @param e the exception
@@ -70,8 +86,9 @@ public class VEOError extends Exception {
 
     /**
      * Construct a new VEOError from a particular method giving error number
-     * 
-     * @param classname name of class and method generating the error (must not be null)
+     *
+     * @param classname name of class and method generating the error (must not
+     * be null)
      * @param method method generating error within the class (may be null)
      * @param errno unique error number (must be > 0)
      * @param s a string describing the error (must not be null)
@@ -83,9 +100,11 @@ public class VEOError extends Exception {
     }
 
     /**
-     * Construct a new VEOError from a particular method giving error number and an exception
-     * 
-     * @param classname name of class and method generating the error (must not be null)
+     * Construct a new VEOError from a particular method giving error number and
+     * an exception
+     *
+     * @param classname name of class and method generating the error (must not
+     * be null)
      * @param method method generating error within the class (may be null)
      * @param errno unique error number (must be > 0)
      * @param s a string describing the error (must not be null)
@@ -96,13 +115,14 @@ public class VEOError extends Exception {
         errorId = constErrorId(classname, method, errno); // has to be done twice because you need to call super() first
         this.errno = errno;
     }
-    
+
     /**
-     * Construct a standard format error message. This static method is used when
-     * an Exception is not required to be thrown - e.g. we are just logging
+     * Construct a standard format error message. This static method is used
+     * when an Exception is not required to be thrown - e.g. we are just logging
      * the message
-     * 
-     * @param classname name of class and method generating the error (must not be null)
+     *
+     * @param classname name of class and method generating the error (must not
+     * be null)
      * @param method method generating error within the class (may be null)
      * @param errno unique error number (must be > 0)
      * @param s a string describing the error (must not be null)
@@ -111,13 +131,14 @@ public class VEOError extends Exception {
     public static String errMesg(String classname, String method, int errno, String s) {
         return errMesg(constErrorId(classname, method, errno), s, null);
     }
-    
+
     /**
-     * Construct a standard format error message. This static method is used when
-     * an Exception is not required to be thrown - e.g. we are just logging
+     * Construct a standard format error message. This static method is used
+     * when an Exception is not required to be thrown - e.g. we are just logging
      * the message
-     * 
-     * @param classname name of class and method generating the error (must not be null)
+     *
+     * @param classname name of class and method generating the error (must not
+     * be null)
      * @param method method generating error within the class (may be null)
      * @param errno unique error number (must be > 0)
      * @param s a string describing the error (must not be null)
@@ -139,7 +160,7 @@ public class VEOError extends Exception {
     private static String errMesg(String errId, String s, Exception e) {
         StringBuilder sb = new StringBuilder();
 
-        assert (s != null && ! s.equals("") && !s.trim().equals(" "));
+        assert (s != null && !s.equals("") && !s.trim().equals(" "));
         sb.append(s);
         if (e != null) {
             sb.append(". Cause is: ");
@@ -150,18 +171,20 @@ public class VEOError extends Exception {
         sb.append(")");
         return sb.toString();
     }
-    
+
     /**
-     * Turn the classname, method, and errno into a unique identifier for this error
-     * 
-     * @param classname name of class and method generating the error (must not be null)
+     * Turn the classname, method, and errno into a unique identifier for this
+     * error
+     *
+     * @param classname name of class and method generating the error (must not
+     * be null)
      * @param method method generating error within the class (may be null)
      * @param errno unique error number (must be > 0)
-     * @return 
+     * @return
      */
     private static String constErrorId(String classname, String method, int errno) {
         StringBuilder sb = new StringBuilder();
-        
+
         assert (classname != null & !classname.equals(""));
         assert (errno > 0);
         sb.append(classname);
@@ -185,7 +208,7 @@ public class VEOError extends Exception {
     public String getErrorId() {
         return errorId;
     }
-    
+
     /**
      * Gets the error number to uniquely identify the error. This is deprecated
      * as it is not a unique id and will be removed.
@@ -196,5 +219,5 @@ public class VEOError extends Exception {
     public int getId() {
         return errno;
     }
-*/
+     */
 }
